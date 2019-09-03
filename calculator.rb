aab05f39108c37ac51f1106b9f96810aee6c2a5f
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-03 15:23:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,4 @@ sum = ( first_number + second_number )
 difference =  ( second_number - first_number )
 product = ( first_number * second_number )
 quotient = ( second_number / first_number )
+learn submit
