9f14d754d81d912035dc7aaad280c281fb25d3d4
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-03 15:21:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@ first_number = 2
 second_number = 6
 sum = ( first_number + second_number )
 difference =  ( second_number - first_number )
-product
+product = ( first_number * second_number )
+quotient = ( second_number / )
